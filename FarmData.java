761ba45ba5503b6854be1419af38aeff8596eb4a
Sion Wilks-Boguszewicz <58896718+sioncamara@users.noreply.github.com>
2020-04-23 03:53:39 -0500

    Add files via upload
    added a lot

--- FarmData.java
+++ FarmData.java
@@ -5,28 +5,73 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
+/**
+ * public class that is used to represent the data stored in all 4 types of data reports
+ */
 public class FarmData {
-    private final SimpleStringProperty farmID;
+    private SimpleStringProperty month;
+    private SimpleStringProperty farmID;
     private SimpleIntegerProperty weight;
+    private SimpleStringProperty percent;
+
+    public FarmData(Integer weight, String month) {
+        this.month = new SimpleStringProperty(month);
+        this.weight = new SimpleIntegerProperty(weight);
+    }
+    public FarmData(Integer weight, String month, String percent) {
+        this.month = new SimpleStringProperty(month);
+        this.weight = new SimpleIntegerProperty(weight);
+        this.percent = new SimpleStringProperty(percent);
+    }
 
     public FarmData(String farmID, Integer weight) {
         this.farmID = new SimpleStringProperty(farmID);
         this.weight = new SimpleIntegerProperty(weight);
     }
+    public FarmData(String farmID, Integer weight, String percent) {
+        this.farmID = new SimpleStringProperty(farmID);
+        this.weight = new SimpleIntegerProperty(weight);
+        this.percent = new SimpleStringProperty(percent);
+    }
 
 
+    public void setMonth(String month) {
+        if (this.month == null) this.month = new SimpleStringProperty(month);
+        this.month.set(month);
+    }
+
+    public void getMonth() {
+        if (this.month == null) this.month = new SimpleStringProperty();
+        this.month.get();
+    }
+
+    public StringProperty monthProperty() {
+        return month;
+    }
+
+
+    // methods for farmID field
     public void setFarmID(String farmID) {
+        if(this.farmID == null) this.farmID = new SimpleStringProperty(farmID);
         this.farmID.set(farmID);
     }
 
     public String getFarmID() {
+        if(this.farmID == null) this.farmID = new SimpleStringProperty();
         return farmID.get();
     }
 
-    public StringProperty getF(){
+    public SimpleStringProperty farmIDProperty() {
         return farmID;
     }
 
+
+    // methods for weight field
+
+    /**
+     * sets weight will never be null since no every constructor required a weight
+     * @param weight
+     */
     public void setWeight(Integer weight) {
         this.weight.set(weight);
     }
@@ -35,7 +80,35 @@ public class FarmData {
         return weight.get();
     }
 
-//    public String toString() {
-//        return getFarmID() + " : " + getWeight();
-//    }
+    public SimpleIntegerProperty weightProperty() {
+        return weight;
+    }
+
+
+    // methods for percent field
+
+
+    public void setPercent(String percent) {
+        if(this.percent == null) this.percent = new SimpleStringProperty();
+        this.percent.set(percent);
+    }
+
+    public String getPercent() {
+        return percent.get();
+    }
+
+    public SimpleStringProperty percentProperty() {
+        return percent;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        if(farmID != null) s += "FarmID " + farmID.get() + "   ";
+        if(month != null) s += "Month " + month.get() + "   ";
+        if(weight != null) s += "Weight " + weight.get() + "   ";
+        if(percent != null) s += "Percent " + percent.get() + "   ";
+
+        return s;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,56 +1,113 @@
 package application;
 
 import javafx.application.Application;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 
+import java.io.File;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static javafx.scene.control.cell.ChoiceBoxTableCell.forTableColumn;
+
 public class Main extends Application {
-    private  int farmId; // used to store farmId user entered
-    private  int year; // used to store year user entered
-    private  int month; // used to store month user entered
+    private int farmId; // used to store farmId user entered
+    private int year; // used to store year user entered
+    private int month; // used to store month user entered
+
+    Scene mainScene;
+
+    private Label minMaxOrAve  = new Label();
 
     private TableView<FarmData> table;
     private ObservableList<FarmData> data = FXCollections.observableArrayList();
+    private Text txtStatus;
+
 
 
     private static final int WINDOW_WIDTH = 700;
     private static final int WINDOW_HEIGHT = 700;
-    private static final String APP_TITLE = "Hello World!";
+    private int leftCompWidth = 200;
+    private int prefButtonWidth = 200;
+    private static final String APP_TITLE = "Milk Weight Analyzer";
 
-
+private VBox reportHolder;
     @Override
     public void start(Stage primaryStage) throws Exception {
 
 
         // load button for top component
         VBox loadContaner = new VBox();
-        Button loadBut = new Button("Load");
+        Label dragFile = new Label("Drag and Drop File Here");
+        dragFile.setId("drop");
+        dragFile.setPadding(new Insets(10,10,0,10));
+        dragFile.setOnDragOver(event -> {
+            if(event.getDragboard().hasFiles()){
+                event.acceptTransferModes(TransferMode.COPY);
+            }
+        });
+        dragFile.setOnDragDropped(event -> {
+            List<File> files = event.getDragboard().getFiles();
+            String filePath = new String(files.get(0).getAbsolutePath());
+            System.out.println(filePath);
+        });
         Separator separator1 = new Separator();
         separator1.setMaxWidth(300);
-        loadContaner.getChildren().addAll(loadBut, separator1);
+        minMaxOrAve.setId("minMaxOrAve");
+        minMaxOrAve.setPadding(new Insets(5,0,0,300));
+        loadContaner.getChildren().addAll(dragFile,minMaxOrAve, separator1);
+
+        // make left component
+        AtomicBoolean dragging = new AtomicBoolean(false);
+        TabPane leftComponent = makeLeftComponent();
+        leftComponent.setPrefWidth(leftCompWidth);
+       // leftComponent.setOnMousePressed(event -> dragging.set(true));
+
+        leftComponent.setOnMouseMoved(event -> {
+            if(event.getX() < leftCompWidth - 15 || event.getX() > leftCompWidth + 15) mainScene.setCursor(Cursor.DEFAULT);
+            else mainScene.setCursor(Cursor.H_RESIZE);
+        });
+        leftComponent.setOnMouseDragged(event -> {
+            if(mainScene.getCursor().equals(Cursor.H_RESIZE) && event.getX() > 10 && event.getX() < WINDOW_WIDTH - 10) leftCompWidth = (int) event.getX();
+             leftComponent.setPrefWidth(leftCompWidth);
 
 
+        });
+      //  leftComponent.setOnMouseReleased(event -> dragging.set(false));
         // Main layout is Border Pane example (top,left,center,right,bottom)
         BorderPane root = new BorderPane();
 
         // add top, left, center, right, and bottom components
         root.setTop(loadContaner);
-        root.setLeft(makeLeftComponent());
+
+        root.setLeft(leftComponent);
         makeTable();
+        table.setOnMouseMoved(event -> {
+
+            if(event.getX() > 15 ) mainScene.setCursor(Cursor.DEFAULT);
+        });
         root.setCenter(table);
-        Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+         mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        mainScene.getStylesheets().add(getClass().getResource("application..css").toExternalForm());
 
         // Add the stuff and set the primary stage
         primaryStage.setTitle(APP_TITLE);
@@ -58,60 +115,129 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-    private void makeTable(){
+    private void makeTable() {
         table = new TableView<FarmData>();
 
-        TableColumn<FarmData,String> farmIDCol = new TableColumn<>("FarmID");
+        TableColumn<FarmData, String> monthCol = new TableColumn<>("Month");
+        monthCol.setCellValueFactory(new PropertyValueFactory<FarmData, String>("month"));
+
+        TableColumn<FarmData, String> farmIDCol = new TableColumn<>("FarmID");
         farmIDCol.setCellValueFactory(new PropertyValueFactory<FarmData, String>("farmID"));
 
-        TableColumn<FarmData,Integer> weightCol = new TableColumn<>("Weight");
+        TableColumn<FarmData, Integer> weightCol = new TableColumn<>("Weight");
         weightCol.setCellValueFactory(new PropertyValueFactory<FarmData, Integer>("weight"));
 
-        table.getColumns().setAll(farmIDCol,weightCol);
+        TableColumn<FarmData, Integer> percentCol = new TableColumn<>("Percent");
+        percentCol.setCellValueFactory(new PropertyValueFactory<FarmData, Integer>("percent"));
 
 
+
+        table.getColumns().setAll(monthCol, farmIDCol,weightCol, percentCol);
+        table.getColumns().get(3).setVisible(false);
 
         // set table properties
         table.setPrefWidth(400);
         table.setPrefHeight(250);
         table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-        table.setPadding(new Insets(20,20,20,20));
+        table.getSelectionModel().selectedIndexProperty().
+                addListener(new RowChangeHandler());
+        table.setEditable(true);
+        farmIDCol.setCellFactory(((TextFieldTableCell.forTableColumn())));
+        weightCol.setOnEditCommit(event -> (event.getTableView().
+                getItems().get(event.getTablePosition().getRow())).
+                setWeight(event.getNewValue()));
+        txtStatus = new Text();
+
+
+        table.setPadding(new Insets(20, 20, 20, 20));
 
     }
 
+    private class RowChangeHandler implements
+            ChangeListener {
+        @Override
+        public void changed(ObservableValue ov, Object oldVal,
+                            Object newVal) {
+            int val = ((Number)newVal).intValue();
+            if (data.size()<=0) {
+                return;
+            }
+            FarmData farmData = (FarmData) data.get(val);
+            txtStatus.setText(farmData.toString());
+        }
+    }
 
-    private void dummyData(){
-        data.add(new FarmData("1",2000));
-        data.add(new FarmData("2",6000));
-        data.add(new FarmData("3",3000));
-        data.add(new FarmData("4",4000));
+
+
+    private void dummyData() {
+        data.add(new FarmData( "farm1", 2000,"5%"));
+        data.add(new FarmData( "2", 6000, "20%"));
+        data.add(new FarmData("3", 3000, "15%"));
+        data.add(new FarmData("4", 4000));
+        data.get(3).setPercent("50%");
+        data.add(new FarmData("farm5", 40484800));
+    }
+
+    private void determineMax(){
+        try {
+            FarmData max = data.stream().max(Comparator.comparing(FarmData::getWeight)).get();
+            minMaxOrAve.setText("Max : " + max);
+        } catch (NoSuchElementException ignored){
+
+        }
+
+    }
+
+    private void determineMin(){
+        try {
+            FarmData min = data.stream().min(Comparator.comparing(FarmData::getWeight)).get();
+            minMaxOrAve.setText("Min : " + min);
+        } catch (NoSuchElementException ignored){
+
+        }
+    }
+
+    private void determineAverage(){
+
+        if(data.size() == 0) return;
+
+        double sum = 0;
+        for(int i = 0; i < data.size(); i++){
+            sum += data.get(i).getWeight();
+        }
+
+        minMaxOrAve.setText("Average weight : " + sum/data.size());
     }
 
     private void farmReport() {
-        table.setVisible(false);
+        table.getColumns().get(0).setVisible(true);
+        table.getColumns().get(1).setVisible(false);
     }
 
     private void annualReport() {
         table.setVisible(true);
+        table.getColumns().get(0).setVisible(false);
+        table.getColumns().get(1).setVisible(true);
+
         data = FXCollections.observableArrayList();
         table.setItems(data);
 
-      dummyData();
-       data.add(new FarmData("Sion's farm", 4558858));
+        dummyData();
+        data.add(new FarmData("Sion's farm", 4558858));
     }
 
-    private void monthlyReport(){
+    private void monthlyReport() {
 
     }
 
-    private void dateRangeReport(){
-        
-    }
+    private void dateRangeReport() {
 
+    }
 
 
     /**
      * create the gui for the left component
+     *
      * @return
      */
     private TabPane makeLeftComponent() {
@@ -127,10 +253,10 @@ public class Main extends Application {
     }
 
     private VBox makeViewTab() {
-        final ToggleGroup group = new ToggleGroup();
+
         VBox viewComponent = new VBox(); // holds view and Edit button
         viewComponent.setStyle("-fx-background-color: #FFFFFF;");
-        viewComponent.setPrefWidth(200);
+       // viewComponent.setPrefWidth(200);
         viewComponent.setMaxHeight(600);
         viewComponent.setPadding(new Insets(0, 10, 10, 10));
 
@@ -138,49 +264,109 @@ public class Main extends Application {
         Insets inputPadd = new Insets(10, 0, 0, 0);
         Label farmId = new Label("Farm ID");
         TextField farmIdInfo = new TextField();
-        farmId.setPadding(new Insets(50, 0, 0, 0));
-        farmIdInfo.setMaxWidth(100);
+        //farmIdInfo.setPadding(new Insets(500));
+        farmIdInfo.setMaxWidth(Double.MAX_VALUE);
 
 
         Label year = new Label("Year");
         year.setPadding(inputPadd);
         TextField yearInput = new TextField();
-        yearInput.setMaxWidth(100);
+        yearInput.setMaxWidth(Double.MAX_VALUE);
 
         Label month = new Label("Month");
         month.setPadding(inputPadd);
         TextField monthIn = new TextField();
-        monthIn.setMaxWidth(100);
+        monthIn.setMaxWidth(Double.MAX_VALUE);
 
+        VBox searchDataHolder = new VBox();
+        searchDataHolder.getChildren().addAll(farmId,farmIdInfo,year,yearInput,month,monthIn);
+        searchDataHolder.setAlignment(Pos.CENTER_LEFT);
+        searchDataHolder.setPadding(new Insets(0,60,0,20));
 
 
         // make radio buttons for diff views
-        RadioButton displayTog = new RadioButton("Display %");
-        displayTog.setPadding(new Insets(20, 0, 0, 0));
-        RadioButton minTog = new RadioButton("Min");
-        RadioButton maxTog = new RadioButton("Max");
-        RadioButton averageTog = new RadioButton("Average");
-        averageTog.setPadding(new Insets(0, 0, 150, 0));
+        VBox hboxHolder = new VBox();
+        HBox percentHolder = new HBox();
+        HBox minHolder = new HBox();
+        HBox maxHolder = new HBox();
+        HBox averageHolder = new HBox();
+
+        Label percentL = new Label("display %");
+        Label minL = new Label("Min");
+        Label maxL = new Label("Max");
+        Label averageL = new Label("Average");
+
+        ToggleButton percentTog = new ToggleButton();
+        percentTog.setOnAction(event -> {if (percentTog.isSelected()) table.getColumns().get(3).setVisible(true);
+        else table.getColumns().get(3).setVisible(false);
+            System.out.println(data.get(0).getFarmID());});
+
+
+
+        ToggleButton minTog = new ToggleButton();
+        minTog.setOnAction(event -> {if (minTog.isSelected()) determineMin();
+        else minMaxOrAve.setText("");});
+
+        ToggleButton maxTog = new ToggleButton();
+        maxTog.setOnAction(event -> {if (maxTog.isSelected()) determineMax();
+        else minMaxOrAve.setText("");});
+
+        ToggleButton averageTog = new ToggleButton();
+        averageTog.setOnAction(event -> {if (averageTog.isSelected()) determineAverage();
+        else minMaxOrAve.setText("");});
+
+        // creating region for spacing of label and button
+        Region spacing1 = new Region();
+        Region spacing2 = new Region();
+        Region spacing3 = new Region();
+        Region spacing4 = new Region();
+        HBox.setHgrow(spacing1, Priority.ALWAYS);
+        HBox.setHgrow(spacing2, Priority.ALWAYS);
+        HBox.setHgrow(spacing3, Priority.ALWAYS);
+        HBox.setHgrow(spacing4, Priority.ALWAYS);
+
+        percentHolder.getChildren().addAll(percentL,spacing1,percentTog);
+        percentHolder.setPadding(new Insets(10,10,15,10));
+        minHolder.getChildren().addAll(minL,spacing2,minTog);
+        minHolder.setPadding(new Insets(0,10,0,10));
+        maxHolder.getChildren().addAll(maxL,spacing3,maxTog);
+        maxHolder.setPadding(new Insets(0,10,0,10));
+        averageHolder.getChildren().addAll(averageL,spacing4,averageTog);
+        averageHolder.setPadding(new Insets(0,10,0,10));
+
+        hboxHolder.getChildren().addAll(percentHolder,minHolder,maxHolder,averageHolder);
+        hboxHolder.setPadding(new Insets(20));
+        hboxHolder.setSpacing(5);
+
+
+
+       // add min max and average to group
+
+
+        ToggleGroup group = new ToggleGroup();
         minTog.setToggleGroup(group);
         maxTog.setToggleGroup(group);
         averageTog.setToggleGroup(group);
 
 
         // report type buttons
-        VBox reportHolder = new VBox(5);
+         reportHolder = new VBox(5);
+        reportHolder.setPadding(new Insets(50, 0, 10, 0));
 
         Button farmReport = new Button("Farm Report");
-        farmReport.setPrefWidth(200);
+      //  farmReport.setPrefWidth(prefButtonWidth);
+        farmReport.setMaxWidth(Double.MAX_VALUE);
         farmReport.setOnAction(event -> farmReport());
 
         Button annualReport = new Button("Annual Report");
-        annualReport.setPrefWidth(200);
+        annualReport.setMaxWidth(Double.MAX_VALUE);
         annualReport.setOnAction(event -> annualReport());
 
         Button monthReport = new Button("Monthly Report");
-        monthReport.setPrefWidth(200);
+        monthReport.setMaxWidth(Double.MAX_VALUE);
         reportHolder.getChildren().addAll(farmReport, annualReport, monthReport);
-        reportHolder.setPadding(new Insets(0, 0, 10, 0));
+       // HBox.setHgrow(farmReport, Priority.ALWAYS);
+
 
         // make start date and end date textfields
         VBox startHolder = new VBox(5);
@@ -188,30 +374,31 @@ public class Main extends Application {
         HBox datesHold = new HBox(5);
         Label start = new Label("Start Date");
         TextField startDate = new TextField();
-        startDate.setPrefWidth(90);
+        startDate.setMaxWidth(Double.MAX_VALUE);
         TextField endDate = new TextField();
         Label end = new Label("End Date");
         Label dash = new Label("-");
         dash.setPadding(new Insets(25, 5, 0, 5));
-        endDate.setPrefWidth(90);
+        endDate.setMaxWidth(Double.MAX_VALUE);
         startHolder.getChildren().addAll(start, startDate);
         startHolder.setPadding(new Insets(0, 0, 0, 10));
         endHolder.getChildren().addAll(end, endDate);
         endHolder.setPadding(new Insets(0, 10, 0, 0));
         datesHold.getChildren().addAll(startHolder, dash, endHolder);
+        datesHold.setAlignment(Pos.BASELINE_CENTER);
+        datesHold.setMaxWidth(Double.MAX_VALUE);
 
 
         // report data range button
         VBox rangeHolder = new VBox();
         Button rangeReport = new Button("Date Range Report");
-        rangeReport.setPrefWidth(200);
+        rangeReport.setMaxWidth(Double.MAX_VALUE);
         rangeHolder.getChildren().add(rangeReport);
         rangeHolder.setPadding(new Insets(5, 0, 0, 0));
 
-        viewComponent.getChildren().addAll(farmId, farmIdInfo, year, yearInput, month, monthIn, displayTog, minTog, maxTog, averageTog, reportHolder, datesHold, rangeHolder);
+        viewComponent.getChildren().addAll(searchDataHolder, hboxHolder, reportHolder, datesHold, rangeHolder);
         return viewComponent;
     }
-
 
 
     private VBox makeEditTab() {
@@ -221,39 +408,38 @@ public class Main extends Application {
         editComponent.setMaxHeight(600);
         editComponent.setPadding(new Insets(0, 10, 10, 30));
         Label instr = new Label("11/07/1999 format");
-        instr.setPadding(new Insets(50,0,0,0));
+        instr.setPadding(new Insets(50, 0, 0, 0));
 
         // farm year and month input
         Insets inputPadd = new Insets(10, 0, 0, 0);
         Label farmId = new Label("Farm ID");
         TextField farmIdInfo = new TextField();
         farmId.setPadding(new Insets(50, 0, 0, 0));
-        farmIdInfo.setMaxWidth(100);
+        farmIdInfo.setMaxWidth(Double.MAX_VALUE);;
 
 
         Label date = new Label("Date");
         date.setPadding(inputPadd);
         TextField dateIn = new TextField();
-        dateIn.setMaxWidth(100);
+        dateIn.setMaxWidth(Double.MAX_VALUE);;
 
         Label weight = new Label("Weight");
         weight.setPadding(inputPadd);
         TextField weightInput = new TextField();
-        weightInput.setMaxWidth(100);
+        weightInput.setMaxWidth(Double.MAX_VALUE);;
 
         // report data range button
         VBox addHolder = new VBox();
         Button addData = new Button("Add Data");
-      //  addData.setPrefWidth(50);
+        // addData.setPrefWidth(50);
         addHolder.getChildren().add(addData);
         addHolder.setPadding(new Insets(30, 0, 0, 0));
 
-        editComponent.getChildren().addAll(instr, farmId, farmIdInfo, date, dateIn, weight, weightInput,addHolder);
+        editComponent.getChildren().addAll(instr, farmId, farmIdInfo, date, dateIn, weight, weightInput, addHolder);
+        editComponent.setPadding(new Insets(0,60,0,30));
 
         return editComponent;
     }
-
-
 
 
     /**
